7c7523de31b3b1c42bee06aced4a1566f06750ce
= <=>
2020-05-01 13:42:54 +0700

    imam commit

---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,5 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2020 <a href="#">Alumni Universitas Lampung</a>.</strong> All rights reserved.
+</footer>
