b73d15515d27d83504ad3ad5da6356ddb3d74554
Tomi007 <tomierfanda007@gmail.com>
2020-05-08 23:32:51 +0700

    tusboll

--- app/Users_Attribut.php
+++ app/Users_Attribut.php
@@ -8,10 +8,6 @@ class Users_Attribut extends Model
 {
     //
     protected $table = 'users_attributs';
-    protected $fillable = ['nama', 'tempat_lahir', 'status' ,'no_hp' ,'tgl_lahir', 'alamat' , 'jurusan_prodi' , 'th_masuk' , 'th_lulus' , 'tempat_bekerja' , 'waktu_lulus_bekerja' , 'path_foto'];
-<<<<<<< HEAD
-
-=======
+    protected $fillable = ['id','nama', 'tempat_lahir', 'status' ,'no_hp' ,'tgl_lahir', 'alamat' , 'jurusan_prodi' , 'th_masuk' , 'th_lulus' , 'tempat_bekerja' , 'waktu_lulus_bekerja' , 'path_foto'];
     public $incrementing = false;
->>>>>>> 80a134672e5cff95b5e9e65f1b34af8596066dfe
 }
