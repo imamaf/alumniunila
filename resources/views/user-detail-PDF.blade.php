7fbebedf480b198453538ef011cf35c40c7d5f1b
= <=>
2020-06-06 13:44:04 +0700

    UI Cetak Pdf

--- resources/views/user-detail-PDF.blade.php
+++ resources/views/user-detail-PDF.blade.php
@@ -1,10 +1,102 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Hi</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Cetak</title>
+
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <!-- CSS Files -->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="{{ asset('css/cetak.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div class="form-group">
+
+    <div class="container">
+        <div class="header">
+            <img src="{{url('img/logo-unila.png')}}" alt="Logo">
+            <div class="header-title">
+                <h3>Sistem Informasi Alumni</h3>
+                <h5>Universitas Lampung</h5>
+            </div>
+        </div>
+        <hr>
+
+        <div class="row">
+            <div class="col-md-12 main">
+                <img src="{{url('img/img-cetak.jpg')}}" alt="Logo">
+                <h1>{{$users_attribut->nama}}</h1>
+            </div>
+        </div>
+
+        <div class="main-content">
+            <div class="main-title">
+                <div class="row">
+                    <div class="col-md-4">
+                        <h5>Tempat Lahir</h5>
+                        <p>{{$users_attribut->tempat_lahir}}</p>
+                    </div>
+                    <div class="col-md-4">
+                        <h5>Tanggal Lahir</h5>
+                        <p>{{$users_attribut->tgl_lahir}}</p>
+                    </div>
+                    <div class="col-md-4">
+                        <h5>Alamat</h5>
+                        <p>{{$users_attribut->alamat}}</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="main-title-2">
+                <div class="row">
+                    <div class="col-md-4">
+                        <h5>No.Handphone</h5>
+                        <p>{{$users_attribut->no_hp}}</p>
+                    </div>
+                    <div class="col-md-4">
+                        <h5>Jurusan/Prodi</h5>
+                        <p>{{$users_attribut->jurusan_prodi}}</p>
+                    </div>
+                    <div class="col-md-4">
+                        <h5>Tahun Masuk</h5>
+                        <p>{{substr($users_attribut->th_masuk,0,4)}}</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="main-title-3">
+                <div class="row">
+                    <div class="col-md-4">
+                        <h5>Tahun Lulus</h5>
+                        <p>{{substr($users_attribut->th_lulus,0,4)}}</p>
+                    </div>
+                    <div class="col-md-4">
+                        <h5>Status</h5>
+                        <p>{{$users_attribut->status}}</p>
+                    </div>
+                    <div class="col-md-4">
+                        <h5>Tempat Bekerja</h5>
+                        <p>{{$users_attribut->tempat_bekerja}}</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="main-title-4">
+                <div class="row">
+                    <div class="col-md-4">
+                        <h5>Waktu Lulus Kerja</h5>
+                        <p>{{$users_attribut->waktu_lulus_bekerja}}</p>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- <div class="form-group">
         <input type="text" class="form-control" id="nama" placeholder="nama" name="nama" value="{{$users_attribut->nama}}">
     </div>
     <div class="form-group">
@@ -37,7 +129,7 @@
     <div class="form-group">
         <input type="text" class="form-control" id="status" placeholder="waktu lulus kerja" name="waktu_lulus_bekerja" value="{{substr($users_attribut->waktu_lulus_bekerja,0,4)}}">
     </div>
-</div>
+</div> -->
 
 </body>
 </html>
